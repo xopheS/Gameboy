be9168e04813d01b4d06de34d6ef6e1300c77572
xopheS <xophesaad@gmail.com>
2018-05-07 11:12:43 +0200

    Started step 11

---
+++ src/ch/epfl/gameboj/gui/ImageConverter.java
@@ -0,0 +1,32 @@
+package ch.epfl.gameboj.gui;
+
+import ch.epfl.gameboj.component.lcd.LcdController;
+import ch.epfl.gameboj.component.lcd.LcdImage;
+
+
+public class ImageConverter {
+    
+    public static javafx.scene.image.Image convert(LcdImage image){
+        
+        WritableImage wImage = new WritableImage(LcdController.LCD_WIDTH, LcdController.LCD_WIDTH);
+        PixelWriter pWriter = wImage.getPixelWriter();
+        
+        for (int y = 0; y < image.getHeight(); ++y) {
+            
+            for (int x = 0; x < image.getWidth(); ++x) {
+                
+                int color = 0;
+                switch(image.get(x, y)) {
+                  case 0 : color = 0xFFFFFFFF; break;
+                  case 1 : color = 0xFFD3D3D3; break;
+                  case 2 : color = 0xFFA9A9A9; break;
+                  case 3 : color = 0xFF000000; break;
+                }
+                pWriter.setArgb(x, y, color);
+            }
+        }
+        
+        
+        
+    }
+}
---
+++ src/ch/epfl/gameboj/gui/Main.java
@@ -0,0 +1,23 @@
+package ch.epfl.gameboj.gui;
+
+public class Main extends Application {
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+      }
+
+      @Override
+      public void start(Stage primaryStage) {
+        long start = System.nanoTime();
+        AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+          long elapsed = now - start;
+          System.out.printf("Temps écoulé : %.2f s%n",
+                    elapsed / 1e9);
+        }
+          };
+        timer.start();
+      }
+
+}
