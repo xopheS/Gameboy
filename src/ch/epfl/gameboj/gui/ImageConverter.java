f3576ab72716e027563b4154fb2e64989820a886
xopheS <xophesaad@gmail.com>
2018-05-25 09:14:07 +0200

    Changed ImageConverter class

--- src/ch/epfl/gameboj/gui/ImageConverter.java
+++ src/ch/epfl/gameboj/gui/ImageConverter.java
@@ -14,23 +14,8 @@ import javafx.scene.image.WritableImage;
  */
 public final class ImageConverter {
 
-    private enum JavaFXColor {
-        COLOR0(0xFF, 0xFF, 0xFF, 0xFF), COLOR1(0xFF, 0xD3, 0xD3, 0xD3), COLOR2(0xFF, 0xA9, 0xA9, 0xA9), COLOR3(0xFF,
-                0x00, 0x00, 0x00);
 
-        private int alpha, red, green, blue;
-
-        JavaFXColor(int alpha, int red, int green, int blue) {
-            this.alpha = alpha;
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
-        }
-
-        int getARG_B() {
-            return alpha << Byte.SIZE * 3 | red << Byte.SIZE * 2 | green << Byte.SIZE | blue;
-        }
-    }
+  public static int[] JavaFXColor = new int[] {0xFFFFFFFF, 0xFFD3D3D3, 0xFFA9A9A9, 0xFF000000};
 
   /**
    * Cette méthode convertit l'image donnée en argument en image JavaFx
@@ -40,32 +25,19 @@ public final class ImageConverter {
    * @return une image JavaFx
    */
     public static Image convert(LcdImage lcdImage) {
-        WritableImage wImage = new WritableImage(lcdImage.getWidth(), lcdImage.getHeight());
-        PixelWriter pWriter = wImage.getPixelWriter();
+      WritableImage wImage = new WritableImage(lcdImage.getWidth(), lcdImage.getHeight());
+      PixelWriter pWriter = wImage.getPixelWriter();
 
-        for (int y = 0; y < lcdImage.getHeight(); ++y) {
-            for (int x = 0; x < lcdImage.getWidth(); ++x) {
-                int color = 0;
+      for (int y = 0; y < lcdImage.getHeight(); ++y) {
 
-                switch (lcdImage.get(x, y)) {
-                case 0:
-                    color = JavaFXColor.COLOR0.getARG_B();
-                    break;
-                case 1:
-                    color = JavaFXColor.COLOR1.getARG_B();
-                    break;
-                case 2:
-                    color = JavaFXColor.COLOR2.getARG_B();
-                    break;
-                case 3:
-                    color = JavaFXColor.COLOR3.getARG_B();
-                    break;
-                }
+          for (int x = 0; x < lcdImage.getWidth(); ++x) {
 
-                pWriter.setArgb(x, y, color);
-            }
-        }
-        
-        return wImage;
-    }
+              int color = JavaFXColor[lcdImage.get(x, y)];
+
+              pWriter.setArgb(x, y, color);
+          }
+      }
+
+      return wImage;
+  }
 }
